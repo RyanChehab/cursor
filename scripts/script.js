6d038f4e92c6968652135a89b4c649ef62e9e751
unknown <ryanchehab20@gmail.com>
2024-10-26 23:54:42 +0200

    finished the code

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,53 @@
 let boundaries = document.querySelectorAll('.boundary');
-boundaries.forEach(boundary=>{
-    boundary.addEventListener('mouseover',function(){
-        boundary.style.color = 'red'
-    })
+let start = document.getElementById('start')
+let end = document.getElementById('end')
+
+let gameLost = false;
+
+// starting the game
+start.addEventListener("mouseover",function(){
+    gameLost=false
+
+    start.style.color = 'green';
+    start.style.background = 'black'
+    let status = document.getElementById('status')
+    status.innerHTML = 'GAME STARTED'
+
+
+
+        // boundaries to red
+
+        boundaries.forEach(boundary=>{
+            boundary.addEventListener('mouseover',function(){
+                if (gameLost===false){
+                gameLost = true;
+                boundaries.forEach(sides=>{
+                    sides.style.background = 'red'})
+
+                let status = document.getElementById('status')
+                status.innerHTML='YOU LOST!'
+                status.style.color= 'red'
+                }
+            })
+        })
+
+            // End
+            end.addEventListener('mouseover',function(){
+                if(gameLost===false){
+                    console.log("wo")
+                status.innerHTML = 'YOU WON'
+                status.style.color = 'green'
+                gameLost=true
+
+                // boundaries to green
+                boundaries.forEach(boundary=>{
+                    boundary.style.background = 'green'
+                })
+                }
+            })
+
+                    
+                
+
+
 })
